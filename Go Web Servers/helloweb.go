202490bd024df5d2aea322b25fd46583f4fb7bb0
mixologee <jayscheponik@gmail.com>
2022-07-12 14:51:50 -0400

    Update helloweb.go

--- Go Web Servers/helloweb.go
+++ Go Web Servers/helloweb.go
@@ -4,11 +4,16 @@ import (
         "net/http"
 )
 
+// function to handle web requests
 func helloWeb(w http.ResponseWriter, r *http.Request) {
+        // response to web request
         w.Write([]byte("Hello Web!\r\n"))
 }
 
+// main function
 func main() {
+        // assign the handler function to a path
         http.HandleFunc("/", helloWeb)
+        // start listening for web requests on a specific port
         http.ListenAndServe(":8000", nil)
 }
