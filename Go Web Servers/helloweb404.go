9410e26081d2358ac1342b6dfa8e1a0f61240f57
mixologee <jayscheponik@gmail.com>
2022-07-12 14:53:21 -0400

    Update helloweb404.go

--- Go Web Servers/helloweb404.go
+++ Go Web Servers/helloweb404.go
@@ -5,14 +5,18 @@ import (
 )
 
 func helloWeb(w http.ResponseWriter, r *http.Request) {
+        // check to see if the path is anything but /, if so throw 404 error
         if r.URL.Path != "/" {
                 http.NotFound(w,r)
                 return
         }
+        // response to web request
         w.Write([]byte("Hello Web!\r\n"))
 }
 
 func main() {
+        // assign the handler function to a path
         http.HandleFunc("/", helloWeb)
+        // start listening for web requests on a specific port
         http.ListenAndServe(":8000", nil)
 }
