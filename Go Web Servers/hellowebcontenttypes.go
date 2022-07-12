b07af01f17ff67f8b814e90d1241d1b99d98fdf3
mixologee <jayscheponik@gmail.com>
2022-07-12 14:55:15 -0400

    Update hellowebcontenttypes.go

--- Go Web Servers/hellowebcontenttypes.go
+++ Go Web Servers/hellowebcontenttypes.go
@@ -5,6 +5,7 @@ import (
 )
 
 func helloWeb(w http.ResponseWriter, r *http.Request) {
+        // send 404 for anything beyond the landing page
         if r.URL.Path != "/" {
                 http.NotFound(w,r)
                 return
@@ -12,18 +13,26 @@ func helloWeb(w http.ResponseWriter, r *http.Request) {
 
         switch r.Header.Get("Accept") {
                 case "application/json":
-                         w.Header().Set("Content-Type", "application/json; characterset=utf-8")
-                         w.Write([]byte(`{"message":"Hello Web!\r\n"}`))
+                        // set a content type in the header
+                        w.Header().Set("Content-Type", "application/json; characterset=utf-8")
+                        // response to web request
+                        w.Write([]byte(`{"message":"Hello Web!\r\n"}`))
                 case "application/xml":
-                         w.Header().Set("Content-Type", "application/xml; characterset=utf-8")
-                         w.Write([]byte(`<?xml version="1.0" encoding="utf-8"?><Message>Hello Web!</Message>`))
+                        // set a content type in the header
+                        w.Header().Set("Content-Type", "application/xml; characterset=utf-8")
+                        // response to web request
+                        w.Write([]byte(`<?xml version="1.0" encoding="utf-8"?><Message>Hello Web!</Message>`))
                 default:
+                        // set a content type in the header
                         w.Header().Set("Content-Type", "text/plain; characterset=utf-8")
+                        // response to web request
                         w.Write([]byte("Hello Web!\r\n"))
         }
 }
 
 func main() {
+        // assign the handler function to a path
         http.HandleFunc("/", helloWeb)
+        // start listening for web requests on a specific port
         http.ListenAndServe(":8000", nil)
 }
