d7efe45622a612a6fee4baad96b746e5aea02a41
mixologee <jayscheponik@gmail.com>
2022-07-14 14:48:55 -0400

    Update simplefilewrite.go

--- WorkingwithFiles/simplefilewrite.go
+++ WorkingwithFiles/simplefilewrite.go
@@ -3,14 +3,13 @@ package main
 import (
         "io/ioutil"
         "log"
-        "fmt"
 )
 
 func main() {
         // create your variable to hold data to be put in the file
-        s := "Hello File!"
+        s := "Hello File!\n"
         // write to the file (file name, content, permissions)
-        err := ioutil.WriteFile("myfile.txt", i[]byte(s), 0644)
+        err := ioutil.WriteFile("myfile.txt", []byte(s), 0644)
         // error handle
         if err != nil {
                 log.Fatal(err)
