d7381d05f771023aa8737e9ace16f8669e82da00
mixologee <jayscheponik@gmail.com>
2022-07-14 14:44:27 -0400

    Update simplefilecreate.go

--- WorkingwithFiles/simplefilecreate.go
+++ WorkingwithFiles/simplefilecreate.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
+	// create your byte variable to hold data to be put in the file
 	b := make([]byte, 0)
+	// write to the file (file name, content, permissions)
 	err := ioutil.WriteFile("myfile.txt", b, 0644)
+	// error handle
 	if err != nil {
 		log.Fatal(err)
 	}
